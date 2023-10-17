618d990967939790644e341b8d20608bcc8e3ca4
ahimze <148057361+ahimze@users.noreply.github.com>
2023-10-17 08:24:36 +0700

    Add files via upload
    belajar dari youtube dan sekolah

---
+++ belajar mulai nol.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void log
+
+int main (){
+cout<< "hello" << ","<< "help me please"<< endl;
+cout << "aowkwowkwok"<< endl;
+
+cout<< "hello" << endl;
+cout<< "yeah" << endl<<endl<<endl;
+cout<< "Name Saya Ahim;" <<endl;
+
+int Umur = 20;
+double Berat = 60.15;
+cout << "Umur saya Adalah "<<Umur<<endl;
+cout << "Berat saya Adalah "<< Berat<< "kg"<< endl;
+
+int x = 5;
+double y = 20.5;
+int z= x * y;
+cout << "kalikan "<< x << " dengan "<<y <<endl;
+cout << "hasil x * y = "<< z;
+
+
+
+}
+
