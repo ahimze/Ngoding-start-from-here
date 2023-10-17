15126f4361c03871f767eaad0329033416e65503
ahimze <148057361+ahimze@users.noreply.github.com>
2023-10-17 08:28:24 +0700

    Add files via upload
    Mulai memahami beberapa struktur pemprogaman

---
+++ Praktikum pertemuan 1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+//tipe data
+	/*
+	string - menuliskan kalimat 
+	integer - bilangan bulat 
+	floating point - bilangan pecahan 
+	double floating point - bilangan pecahan
+	char - character terbatas- 1 huruf
+	bool - kondisi benar atau salah 
+	
+	tipe data nama_variaberl = value variabel
+	*/
+	string nama_saya = "superman";
+	int nomor_int = 10;
+	float nomor_float = 10.1;
+	double nomor_double = 10.2;
+	char karakter = 'A';
+	bool bolean = true;
+	
+	cout<< "string nama_saya  : "<<nama_saya<< endl; 
+	cout<< "nomor int : " <<nomor_int<< endl; 
+	cout<< "nomor float  : " << nomor_float<< endl;
+	cout<< "nomor double : "<< nomor_double<< endl;  
+	cout<< "char : "<<karakter<< endl; 
+	cout<< "bolean  : "<<bolean<< endl; 
+}
