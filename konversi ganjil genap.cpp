45c56ccf0c896723ec77a2f20744a8556b25b439
ahimze <148057361+ahimze@users.noreply.github.com>
2023-10-17 08:40:07 +0700

    Add files via upload
    Membuat Konversi Ganjil Genap

---
+++ konversi ganjil genap.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int x;
+cout<<"========================="<<endl;
+cout<<"==Konversi Ganjil Genap=="<<endl;
+cout<<"========================="<<endl;
+cout<<"masukkan Nilai x = ";cin>>x;
+cout<<"========================="<<endl;
+
+if(x % 2 == 0)
+cout<<"x = genap"<<endl;
+else
+cout<<"x = ganjil"<<endl;
+cout<<"........................."<<endl;
+cout<<"*****Program Selesai*****"<<endl;
+cout<<"........................."<<endl;
+
+
+
+
+
+
+}
+
